4615caae429bd7ca5e6ea9de80c5306896a88192
Noel da Costa <noeldacosta@gmail.com>
2019-05-11 16:08:03 +0100

    Added Dockerfile and kubernites yaml

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var cors = require("cors");
+var app = express();
+
+var corsOptions = {
+  origin: 'http://fe',
+  optionsSuccessStatus: 200
+};
+
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
+
+app.get("/test", cors(corsOptions), (req, res, next) => {
+ res.json({"success": true, "message": "booya!"});
+}, (req, res, next) => {
+  console.log(req)
+  console.log(res);
+});
